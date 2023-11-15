78d2b43c119a14eb3d13d3ac3c49d29ad38337e0
JULIUS NYERERE <nyerereoffice@gmail.com>
2023-11-15 16:32:33 +0300

    Add files via upload

---
+++ django-api/drinksapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    path('drinks/', views.drink_list),
+    path('drinks/<str:id>', views.drink_detail)
+
+]
+
+
+urlpatterns = format_suffix_patterns(urlpatterns)
